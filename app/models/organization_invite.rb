fd5b5febb82c03977f31f745a9690e6e002c4266
smurfik <tammy@mlpinit.com>
2016-05-13 11:44:18 -0700

    add validations for email

--- app/models/organization_invite.rb
+++ app/models/organization_invite.rb
@@ -1,3 +1,6 @@
 class OrganizationInvite < ActiveRecord::Base
+  validates :email, presence: true
+  validates :email, uniqueness: { scope: :organization_id, message: "Person already has been invited." }
+
   belongs_to :organization
 end
