60c895d7bdaacf6204c50923be2fe927e029e0f5
smurfik <tammy@mlpinit.com>
2016-05-16 13:43:43 -0700

    factor out users_org and org_invite

--- app/controllers/organization_invites_controller.rb
+++ app/controllers/organization_invites_controller.rb
@@ -1,5 +1,5 @@
 class OrganizationInvitesController < ApplicationController
-  helper_method :organization
+  helper_method :organization, :org_invite
 
   def new
   end
@@ -7,16 +7,14 @@ class OrganizationInvitesController < ApplicationController
   def create
     redirect_to root_path unless UsersOrganization.find_by!(user: current_user, organization: organization).admin?
     if user
-      @users_org = UsersOrganization.new(user: user, organization: organization, role: params[:organization_invite][:role].to_i, active: true)
-      if @users_org.save
+      if users_org.save
         redirect_to root_path, notice: "Person is invited"
       else
         flash[:notice] = "Person is already part of the organization."
         render :new
       end
     else
-      @org_invite = OrganizationInvite.new(params.require(:organization_invite).permit(:email, :role, :organization_id))
-      if @org_invite.save
+      if org_invite.save
         redirect_to root_path, notice: "Person is invited"
       else
         render :new
@@ -34,5 +32,18 @@ class OrganizationInvitesController < ApplicationController
 
   def org_id
     params[:organization_id] || params[:organization_invite][:organization_id]
+  end
+
+  def users_org
+    @users_org = UsersOrganization.new(
+      user: user,
+      organization: organization,
+      role: params[:organization_invite][:role].to_i,
+      active: true
+    )
+  end
+
+  def org_invite
+    @org_invite = OrganizationInvite.new(params.require(:organization_invite).permit(:email, :role, :organization_id))
   end
 end
