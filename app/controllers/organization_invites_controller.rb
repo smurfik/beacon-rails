a82f22c9943bd55780a19aef4b67564db5e9f790
smurfik <tammy@mlpinit.com>
2016-05-16 12:03:19 -0700

    refactor out user & organization

--- app/controllers/organization_invites_controller.rb
+++ app/controllers/organization_invites_controller.rb
@@ -1,27 +1,33 @@
 class OrganizationInvitesController < ApplicationController
+  helper_method :organization
+
   def new
-    @organization = Organization.find(params[:organization_id])
   end
 
   def create
-    @user = User.find_by(email: params[:organization_invite][:email])
-    if !@user
+    if !user
       @org_invite = OrganizationInvite.new(params.require(:organization_invite).permit(:email, :role, :organization_id))
       if @org_invite.save
         redirect_to root_path, notice: "Person is invited"
       else
-        @organization = Organization.find(params[:organization_invite][:organization_id])
         render :new
       end
     else
-      @users_org = UsersOrganization.new(user: @user, organization_id: params[:organization_invite][:organization_id], role: params[:organization_invite][:role].to_i, active: true)
+      @users_org = UsersOrganization.new(user: user, organization_id: params[:organization_invite][:organization_id], role: params[:organization_invite][:role].to_i, active: true)
       if @users_org.save
         redirect_to root_path, notice: "Person is invited"
       else
-        @organization = Organization.find(params[:organization_invite][:organization_id])
         render :new
         flash[:notice] = "Person is already part of the organization."
       end
     end
+  end
+
+  def user
+    @user ||= User.find_by(email: params[:organization_invite][:email])
+  end
+
+  def organization
+    @organization ||= current_user.organizations.find(params[:organization_id])
   end
 end
