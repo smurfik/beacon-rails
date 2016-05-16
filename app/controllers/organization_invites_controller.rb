3c804dcd13a246916f8b03aeecb18b121afd3bd8
smurfik <tammy@mlpinit.com>
2016-05-16 12:45:43 -0700

    refactor out org_id & user logic

--- app/controllers/organization_invites_controller.rb
+++ app/controllers/organization_invites_controller.rb
@@ -5,21 +5,21 @@ class OrganizationInvitesController < ApplicationController
   end
 
   def create
-    redirect_to root_path unless UsersOrganization.find_by!(user: current_user, organization_id: params[:organization_invite][:organization_id]).admin?
-    if !user
+    redirect_to root_path unless UsersOrganization.find_by!(user: current_user, organization: organization).admin?
+    if user
+      @users_org = UsersOrganization.new(user: user, organization: organization, role: params[:organization_invite][:role].to_i, active: true)
+      if @users_org.save
+        redirect_to root_path, notice: "Person is invited"
+      else
+        flash[:notice] = "Person is already part of the organization."
+        render :new
+      end
+    else
       @org_invite = OrganizationInvite.new(params.require(:organization_invite).permit(:email, :role, :organization_id))
       if @org_invite.save
         redirect_to root_path, notice: "Person is invited"
       else
         render :new
-      end
-    else
-      @users_org = UsersOrganization.new(user: user, organization_id: params[:organization_invite][:organization_id], role: params[:organization_invite][:role].to_i, active: true)
-      if @users_org.save
-        redirect_to root_path, notice: "Person is invited"
-      else
-        render :new
-        flash[:notice] = "Person is already part of the organization."
       end
     end
   end
@@ -29,6 +29,10 @@ class OrganizationInvitesController < ApplicationController
   end
 
   def organization
-    @organization ||= current_user.organizations.find(params[:organization_id])
+    @organization ||= current_user.organizations.find(org_id)
+  end
+
+  def org_id
+    params[:organization_id] || params[:organization_invite][:organization_id]
   end
 end
