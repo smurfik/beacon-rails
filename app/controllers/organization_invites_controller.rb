676b47ca0ec8743454741614d55b83c385d2f6f8
smurfik <tammy@mlpinit.com>
2016-05-18 11:03:49 -0700

    refactor create method

--- app/controllers/organization_invites_controller.rb
+++ app/controllers/organization_invites_controller.rb
@@ -1,49 +1,41 @@
 class OrganizationInvitesController < ApplicationController
-  helper_method :organization, :org_invite
 
   def new
+    @organization ||= current_user.organizations.find(params[:organization_id])
   end
 
   def create
-    redirect_to root_path unless UsersOrganization.find_by!(user: current_user, organization: organization).admin?
-    if user
-      if users_org.save
-        redirect_to root_path, notice: "Person is invited"
-      else
-        flash[:notice] = "Person is already part of the organization."
-        render :new
-      end
+    @organization ||= current_user.organizations.find(params[:organization_invite][:organization_id])
+    redirect_to root_path unless UsersOrganization.find_by!(user: current_user, organization: @organization).admin?
+    @user ||= User.find_by(email: params[:organization_invite][:email].downcase)
+    if @user
+      add_to_org
     else
-      if org_invite.save
-        redirect_to root_path, notice: "Person is invited"
-      else
-        render :new
-      end
+      create_org_invite
     end
   end
 
-  def user
-    @user ||= User.find_by(email: params[:organization_invite][:email].downcase)
-  end
-
-  def organization
-    @organization ||= current_user.organizations.find(org_id)
-  end
-
-  def org_id
-    params[:organization_id] || params[:organization_invite][:organization_id]
-  end
-
-  def users_org
+  def add_to_org
     @users_org = UsersOrganization.new(
-      user: user,
-      organization: organization,
+      user: @user,
+      organization: @organization,
       role: params[:organization_invite][:role],
       active: true
     )
+    if @users_org.save
+      redirect_to root_path, notice: "Person is invited"
+    else
+      flash[:notice] = "Person is already part of the organization."
+      render :new
+    end
   end
 
-  def org_invite
+  def create_org_invite
     @org_invite = OrganizationInvite.new(params.require(:organization_invite).permit(:email, :role, :organization_id))
+    if @org_invite.save
+      redirect_to root_path, notice: "Person is invited"
+    else
+      render :new
+    end
   end
 end
