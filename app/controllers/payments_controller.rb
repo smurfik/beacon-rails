3742928326b04c80735502a74f8d22f6727ddeb4
smurfik <tammy@mlpinit.com>
2016-05-23 10:27:35 -0700

    only create a new stripe customer if org doesn't have stripe id

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,12 +5,16 @@ class PaymentsController < ApplicationController
   def create_customer
     @organization ||= current_user.organizations.find(params[:organization_id])
     redirect_to root_path unless UsersOrganization.find_by!(user: current_user, organization: @organization).admin?
-    @response = Stripe::Customer.create(
-      :description => "Customer for test@example.com",
-      :source => params[:stripeToken]
-    )
-    @organization.update!(stripe_id: @response.id)
-    redirect_to root_path, notice: "Customer was created in Stripe"
+    if @organization.stripe_id.blank?
+      @response = Stripe::Customer.create(
+        :description => "Customer for test@example.com",
+        :source => params[:stripeToken]
+      )
+      @organization.update!(stripe_id: @response.id)
+      redirect_to root_path, notice: "Customer was created in Stripe"
+    else
+      redirect_to root_path, notice: "Organization already has a credit card"
+    end
   end
 
   def create_charge
